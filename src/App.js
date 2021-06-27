132bab905bc4d6f6a82ef252d4582b5cc7d1ecd1
Maruf Khan <whoismaruf@outlook.com>
2021-06-27 15:17:01 +0600

    added new feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h2 className={"text-xl mt-4"}>Hello World!</h2>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
