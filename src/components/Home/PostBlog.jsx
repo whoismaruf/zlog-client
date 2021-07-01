0d2c3b9c670c854f1c7bc6845696c7d1bf226821
whoismaruf <whoismaruf@outlook.com>
2021-07-01 23:42:40 +0600

    prevent app to post blank form-data

--- src/components/Home/PostBlog.jsx
+++ src/components/Home/PostBlog.jsx
@@ -34,11 +34,11 @@ function PostBlog() {
                     <form onSubmit={handleSubmit} action="">
                         <div className="relative mb-4">
                             <label htmlFor="title" className="leading-7 text-sm text-gray-600">Title</label>
-                            <input value={title} onChange={(e) => setTitle(e.target.value)} type="title" id="title" name="title" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                            <input required value={title} onChange={(e) => setTitle(e.target.value)} type="title" id="title" name="title" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                         </div>
                         <div className="relative mb-4">
                             <label htmlFor="body" className="leading-7 text-sm text-gray-600">Body</label>
-                            <textarea value={body} onChange={(e) => setBody(e.target.value)} id="body" name="body" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                            <textarea required value={body} onChange={(e) => setBody(e.target.value)} id="body" name="body" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                         </div>
                         <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Post</button>
                     </form>
