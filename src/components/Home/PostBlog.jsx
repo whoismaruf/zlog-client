5e605c4600560b84afe6a52e4370335ea7ae3978
whoismaruf <whoismaruf@outlook.com>
2021-07-04 23:39:23 +0600

    Extending support for all device

--- src/components/Home/PostBlog.jsx
+++ src/components/Home/PostBlog.jsx
@@ -27,14 +27,14 @@ function PostBlog() {
 
     return (
         <div className={"-mt-24"}>
-            <div className={"container px-5 py-24 mx-auto flex"}>
-                <div className={"lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 "}>
+            <div className={"container px-5 py-24 relative"}>
+                <div className={"lg:w-1/2 md:w-2/3 2xl:1/3 2xl:ml-96 bg-white rounded-lg p-8 flex lg:absolute lg:left-32 flex-col md:ml-32 w-full 2xl:-left-5 3xl:left-32 mt-10 md:mt-0 z-10 "}>
                     {/* <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Create Post</h2>
                     <p className="leading-relaxed mb-5 text-gray-600">Post your thoughts to Zlog community</p> */}
                     <form onSubmit={handleSubmit} action="">
                         <div className="relative mb-4">
                             <label htmlFor="title" className="leading-7 text-sm text-gray-600">Title</label>
-                            <input required value={title} onChange={(e) => setTitle(e.target.value)} type="title" id="title" name="title" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                            <input required value={title} onChange={(e) => setTitle(e.target.value)} type="title" id="title" name="title" className={"w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"}/>
                         </div>
                         <div className="relative mb-4">
                             <label htmlFor="body" className="leading-7 text-sm text-gray-600">Body</label>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
    darkMode: false, // or 'media' or 'class'
    theme: {
-     extend: {},
+     extend: {
+      screens: {
+        '3xl': '1920px',
+      },
+     },
    },
    variants: {
      extend: {},
